dbb9c74777f7bc5679aaa130ed107f8b9ffb3f7b
liuyal <jerrylarryliu@gmail.com>
2025-12-27 11:25:51 -0800

    add prj page

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -7,16 +7,67 @@ import { Observable } from 'rxjs';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <button (click)="refresh()">Refresh</button>
-
-    @if (data$ | async; as data) {
-      <p>{{ data | json }}</p>
-    } @else {
-      <p>Loading...</p>
+  styles: [`
+    .prj-refresh-btn {
+      padding: 10px;
+      width: 40px;
+      height: 40px;
+      border: 1px solid #ccc;
+      border-radius: 5px;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      justify-content: center;
     }
+    .prj-refresh-btn:hover {
+      background-color: #0057adff;
+    }
+    .prj-table {
+      margin-top: 15px; 
+      width: 100%; 
+      border-collapse: collapse;
+      border-radius: 50px;
+    }
+  `],
+  template: `
 
-    
+    <div style="margin: 25px;">
+
+      <button class="prj-refresh-btn" (click)="refresh()">
+        <i class="fa-solid fa-arrows-rotate"></i>
+      </button>
+
+      @if (data$ | async; as data) {
+        <table class="prj-table" border="1">
+          <thead>
+            <tr>
+              <th>PROJECTS</th>
+              <th>DESCRIPTION</th>
+              <th>STATUS</th>
+            </tr>
+          </thead>
+          <tbody>
+            @for (item of data; track item.project_key) {
+              <tr>
+                <td>{{ item.project_key }}</td>
+                <td>{{ item.description }}</td>
+                <td>
+                  @if (item.is_active == true ) {
+                    <i class="fa-solid fa-circle-check" style="color: green;"></i>
+                  } @else {
+                    <i class="fa-solid fa-circle-xmark" style="color: red;"></i>
+                  }
+                </td>
+              </tr>
+            }
+          </tbody>
+        </table>
+
+      } @else {
+        <p>Loading...</p>
+      }
+
+    </div>
   `
 })
 
