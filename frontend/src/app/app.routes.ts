c323203f3b73b0e85dcd114c42070b18d4c8b17f
liuyal <jerrylarryliu@gmail.com>
2025-12-27 12:18:20 -0800

    split pages from app

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { Project } from './projects';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: App },
+  { path: 'projects', component: Project }
+];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,81 +1,13 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule],
-  styles: [`
-    .prj-refresh-btn {
-      padding: 10px;
-      width: 40px;
-      height: 40px;
-      border: 1px solid #ccc;
-      border-radius: 5px;
-      cursor: pointer;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-    .prj-refresh-btn:hover {
-      background-color: #0057adff;
-    }
-    .prj-table {
-      margin-top: 15px; 
-      width: 100%; 
-      border-collapse: collapse;
-      border-radius: 50px;
-    }
-  `],
+  imports: [RouterOutlet],
   template: `
-
-    <div style="margin: 25px;">
-
-      <button class="prj-refresh-btn" (click)="refresh()">
-        <i class="fa-solid fa-arrows-rotate"></i>
-      </button>
-
-      @if (data$ | async; as data) {
-        <table class="prj-table" border="1">
-          <thead>
-            <tr>
-              <th>PROJECTS</th>
-              <th>DESCRIPTION</th>
-              <th>STATUS</th>
-            </tr>
-          </thead>
-          <tbody>
-            @for (item of data; track item.project_key) {
-              <tr>
-                <td>{{ item.project_key }}</td>
-                <td>{{ item.description }}</td>
-                <td>
-                  @if (item.is_active == true ) {
-                    <i class="fa-solid fa-circle-check" style="color: green;"></i>
-                  } @else {
-                    <i class="fa-solid fa-circle-xmark" style="color: red;"></i>
-                  }
-                </td>
-              </tr>
-            }
-          </tbody>
-        </table>
-
-      } @else {
-        <p>Loading...</p>
-      }
-
-    </div>
+    <router-outlet></router-outlet>
   `
 })
 
 export class App {
-  http = inject(HttpClient);
-  data$: Observable<any[]> = this.http.get<any[]>('api/projects');
-  refresh() {
-    this.data$ = this.http.get<any[]>('api/projects');
-  }
 }
-
