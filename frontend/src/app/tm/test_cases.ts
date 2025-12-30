1538c72fafdc03d66f1c47171a56786e369863ee
liuyal <jerrylarryliu@gmail.com>
2025-12-29 16:59:57 -0800

    re-org files

---
+++ frontend/src/app/tm/test_cases.ts
@@ -0,0 +1,29 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'test-cases',
+  standalone: true,
+  imports: [CommonModule],
+  styleUrls: ['./test_cases.css'],
+  templateUrl: './test_cases.html'
+})
+
+export class TestCases {
+  route = inject(ActivatedRoute);
+  router = inject(Router);
+  projectKey = '';
+  activeTab: 'test-cases' | 'test-cycles' = 'test-cases';
+
+  ngOnInit() {
+    this.route.params.subscribe(params => {
+      this.projectKey = params['projectKey'];
+      console.log('Test Cases page for project:', this.projectKey);
+    });
+  }
+
+  goBack() {
+    this.router.navigate(['/projects']);
+  }
+}
