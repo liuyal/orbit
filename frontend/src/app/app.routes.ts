ddfa9c85d75ece681926f4455f3d657b2c6b44f2
liuyal <jerrylarryliu@gmail.com>
2025-12-29 17:29:15 -0800

    Add landing page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { App } from './app';
+import { Landing } from './landing';
 import { Project } from './tm/projects';
 import { TestCases } from './tm/test_cases';
 
 export const routes: Routes = [
-  { path: '', component: App },
+  { path: '', component: Landing },
   { path: 'projects', component: Project },
   { path: 'test-cases/:projectKey', component: TestCases }
 ];
