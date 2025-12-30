4d108318bb9c856a1d08aaba0c6c16bdd77f6f93
liuyal <jerrylarryliu@gmail.com>
2025-12-29 17:08:40 -0800

    remove landing

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import { Landing } from './landing';
+import { App } from './app';
 import { Project } from './tm/projects';
 import { TestCases } from './tm/test_cases';
 
 export const routes: Routes = [
-  { path: '', component: Landing },
+  { path: '', component: App },
   { path: 'projects', component: Project },
   { path: 'test-cases/:projectKey', component: TestCases }
 ];
