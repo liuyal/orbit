d2fb7d642aad9f8cd125e513dfaefaab449a352b
liuyal <jerrylarryliu@gmail.com>
2026-01-10 12:55:55 -0800

    update compose scripts

--- docker/setup.sh
+++ docker/setup.sh
@@ -18,6 +18,8 @@ for ((i=0; i<$#; i++)); do
     BUILD_FLAG="--build"
   elif [[ "$arg" == "--clear" || "$arg" == "-c" ]]; then
     CLEAR_FLAG="--clear"
+   elif [[ "$arg" == "--stop" || "$arg" == "-s" ]]; then
+    STOP_FLAG="--stop"
   elif [[ "$arg" == "--runner" || "$arg" == "-r" ]]; then
     RUNNER_FLAG="--runner"
     # Check if next argument is a number
@@ -36,6 +38,14 @@ if [[ -n "$CLEAR_FLAG" ]]; then
   docker volume prune -af
   echo "Docker cleanup complete"
   exit 0
+
+elif [[ -n "$STOP_FLAG" ]]; then
+  echo "Cleaning up existing containers..."
+  docker stop $(docker ps -q)
+  docker rm -f $(docker ps -aq)
+  echo "Docker containers cleanup complete"
+  exit 0
+
 fi
 
 if [[ -n "$BUILD_FLAG" ]]; then
