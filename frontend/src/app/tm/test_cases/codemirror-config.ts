fdb837c6f21a9dd4cbaf43b7ca88322e58be61cb
liuyal <jerrylarryliu@gmail.com>
2026-01-23 22:42:54 -0800

    add code 6

---
+++ frontend/src/app/tm/test_cases/codemirror-config.ts
@@ -0,0 +1,36 @@
+// CodeMirror 6 configuration for Angular
+import { EditorView, basicSetup } from 'codemirror';
+import { python } from '@codemirror/lang-python';
+import { oneDark } from '@codemirror/theme-one-dark';
+
+export function createCodeMirrorEditor(container: HTMLElement, initialValue: string, onChange: (value: string) => void): EditorView {
+  const editor = new EditorView({
+    doc: initialValue,
+    extensions: [
+      basicSetup,
+      python(),
+      oneDark,
+      EditorView.updateListener.of((update) => {
+        if (update.docChanged) {
+          onChange(update.state.doc.toString());
+        }
+      }),
+      EditorView.theme({
+        '&': {
+          height: '100%',
+          fontSize: '14px'
+        },
+        '.cm-scroller': {
+          overflow: 'auto',
+          fontFamily: "'Consolas', 'Monaco', 'Courier New', monospace"
+        },
+        '.cm-content': {
+          minHeight: '100%'
+        }
+      })
+    ],
+    parent: container
+  });
+
+  return editor;
+}
