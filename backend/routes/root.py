a9a3ca2c6466a92fe0b75554eaa7cc24c5576e59
Jerry Liu <jerry.liu1@se.com>
2026-02-20 10:57:30 -0800

    Update reset APIs

--- backend/routes/root.py
+++ backend/routes/root.py
@@ -12,7 +12,7 @@ import logging
 from fastapi import APIRouter, Request, status, Response
 from fastapi.responses import RedirectResponse
 
-from backend.app.app_def import API_VERSION
+from backend.app.app_def import API_VERSION, DB_COLLECTIONS
 
 router = APIRouter()
 
@@ -41,14 +41,36 @@ async def root_api(request: Request):
     return RedirectResponse(url=f"{base_url}/api/{API_VERSION}/docs")
 
 
-@router.post(f"/api/{API_VERSION}/tm/reset",
+@router.post(f"/api/{API_VERSION}/reset",
              tags=["root"],
              status_code=status.HTTP_204_NO_CONTENT)
-async def reset_tm_server(request: Request):
-    """ Root endpoint to reset server. """
+async def reset_server_db(request: Request):
+    """ Root endpoint to reset server db. """
 
     # Reset the database
     db = request.app.state.mdb
     await db.configure(clean_db=True)
+
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@router.post(f"/api/{API_VERSION}/tm/reset",
+             tags=["root"],
+             status_code=status.HTTP_204_NO_CONTENT)
+async def reset_tm_server_db(request: Request):
+    """ Root endpoint to reset tm server. """
+
+    # Reset the database
+    db = request.app.state.mdb
+
+    # Initialize required collections
+    collections = await db[db.db_name].list_collection_names()
+    for collection, schema in DB_COLLECTIONS:
+        await db[db.db_name].drop_collection(collection)
+        await db[db.db_name].create_collection(collection)
+
+        if collection not in collections:
+            await db[db.db_name].create_collection(collection,
+                                                   validator={"$jsonSchema": schema})
 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
