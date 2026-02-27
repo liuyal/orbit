99cd0b6be28b10214f437d84a0eacf956d549eec
liuyal <jerrylarryliu@gmail.com>
2026-02-26 21:00:05 -0800

    WIP projects page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from '@angular/router';
 import { Landing } from './view/landing/landing';
-import { RunnersStatus } from './view/runners/runners.status';
+import { RunnersStatus } from './view/runners-status/runners.status';
 
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'runner-status', component: RunnersStatus },
-
 ];
