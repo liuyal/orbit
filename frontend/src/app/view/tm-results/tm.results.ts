9cf99f08b62127c348611a35d1ee8e56fd2ec251
liuyal <jerrylarryliu@gmail.com>
2026-06-03 21:33:13 -0700

    create results pages

---
+++ frontend/src/app/view/tm-results/tm.results.ts
@@ -0,0 +1,29 @@
+import { Component, inject, ChangeDetectorRef, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NavbarComponent } from '../../component/navbar/navbar';
+
+@Component({
+    selector: 'app-tm-results',
+    standalone: true,
+    imports: [
+        CommonModule,
+        NavbarComponent
+    ],
+    styleUrls: ['./tm.results.css'],
+    templateUrl: './tm.results.html'
+})
+
+export class TmResults implements OnInit {
+    cdr = inject(ChangeDetectorRef);
+    route = inject(ActivatedRoute);
+    router = inject(Router);
+
+    goBack() {}
+
+    ngOnInit(): void { }
+
+    ngAfterViewInit(): void { }
+
+    ngOnDestroy(): void { }
+}
