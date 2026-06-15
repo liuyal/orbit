80ebad8294840e03f86f03bef8ce6f5672db36ac
Jerry Liu <jerry.liu1@se.com>
2026-06-15 13:57:09 -0700

    Update httpd permission

---
+++ docker/apache-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Ensure the WebDAV upload directory is writable by Apache at runtime
+chmod 777 /usr/local/apache2/htdocs/files
+
+# Hand off to the default Apache entrypoint
+exec httpd-foreground "$@"
+
